7d32eb4db463dee24dd84de5af4f03aa10609d40
Priyanshu11252 <sarojpriyanshu684@gmail.com>
2026-07-13 21:11:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Student Academic Performance Prediction System")
+
+st.success("Project Successfully Deployed")
+
+st.write("Best Model: Random Forest")
+st.write("R² Score: 0.805")
+
+st.write("Dataset: UCI Student Performance Dataset")
